6199e703ffddf92cb9dd4b5557d092cf47c5eae4
Jason Maurer <j@maur.co>
2016-06-30 00:50:17 -0600

    regex rotuer

--- src/router.js
+++ src/router.js
@@ -1,10 +1,78 @@
+import { escapeRegex } from './utils'
+
 /**
- * Creates a route and parses the metadata for it.
- * @param {object} config - An airflow route config
- * @param {object} routeMap - The current server's route map
- * @returns {object} The route info
+ * Converts a segment URL to a regex.
+ * @param {string} url - The url to convert
+ * @returns {string} The splat url
  */
-export function parseRoute (config, routeMap = {}) {
+export function expandUrl (url, esc = true) {
+  if (typeof url !== 'string') {
+    throw new TypeError('URL must be a string')
+  }
+
+  /* split url at each slash */
+  const split = url.split('/')
+
+  /* go through each segment, and convert to regex */
+  return '^' + split.map((seg, idx) => {
+    /* escape regex characters if needed */
+    let val = esc ? escapeRegex(seg) : seg
+
+    /* match segments */
+    if (seg.match(/^:/)) val = '[^\\/]+'
+    /* add end of line flag */
+    if (idx === (split.length - 1)) val += '$'
+
+    return val
+  }).join('\\/')
+}
+
+/**
+ * Extracts segment data from a url using a splat.
+ * @param {string} url - The url to extract from
+ * @param {string} fromUrl - The url pattern to use
+ * @returns {object} The segment data
+ */
+export function getSegmentData (url, fromUrl) {
+  if (typeof url !== 'string' || typeof fromUrl !== 'string') {
+    throw new TypeError('URL must be a string')
+  }
+
+  const urlSplit = url.split('/')
+  const data = {}
+
+  fromUrl.split('/').forEach((seg, indx) => {
+    const match = seg.match(/^:(.*)/)
+    if (match) data[match[1]] = urlSplit[indx]
+  })
+
+  return data
+}
+
+/**
+ * Match a route in the route map.
+ * @param {object} config - The airflow route object
+ * @param {object} routeMap - The global routes map
+ * @returns {object} The route that was found
+ */
+export function lookupRoute (config, routeMap) {
+  const methodMap = routeMap[config.method.toLowerCase()]
+
+  const key = Object.keys(methodMap).find((r) => {
+    return config.url.match(new RegExp(r))
+  })
+
+  return methodMap[key]
+}
+
+/**
+ * Creates a route and adds it to the route map.
+ * @param {object} config - An airflow route config
+ */
+export function createRoute (config) {
+  // TODO: parse config as array(s) as well as object
+  // TODO: check for a valid url
+
   if (!config) {
     throw new TypeError('Routes must have a configuration object')
   }
@@ -12,90 +80,21 @@ export function parseRoute (config, routeMap = {}) {
     throw new TypeError('Route method must be one of [ GET, POST, PUT, PATCH, DELETE ]')
   }
   if (typeof config.url !== 'string') {
-    // TODO: check for a valid url
     throw new TypeError('Route URL must be a string')
   }
   if (typeof config.handler !== 'function') {
     throw new TypeError('Route handler must be a function!')
   }
 
-  /* get route info */
-  const splatter = getSplat(config.url)
+  /* get regex url and and method type */
+  const regexUrl = expandUrl(config.url)
   const method = config.method.toLowerCase()
 
   /* make sure route isn't a duplicate */
-  if (routeMap.routes[method][config.url]) {
+  if (config().routeMap[method][regexUrl]) {
     throw new Error(`"${config.method} ${config.url}" is defined more than once`)
   }
 
-  return {
-    splatter, method,
-    route: config
-  }
-}
-
-/**
- * Looks up a route from the routes map by the splatter.
- * @param {object} config - The airflow route object
- * @param {object} routesMap - The global routes map
- * @returns {object} The route that was found
- */
-export function lookupRoute (config, routesMap) {
-  const methodMap = routesMap.routes[config.method.toLowerCase()]
-
-  let route
-  if (config.url.match(/:/)) {
-    const splatter = getSplat(config.url)
-    route = methodMap[splatter.splat]
-  } else {
-    route = methodMap[config.url]
-  }
-
-  return route
-}
-
-/**
- * Extracts splat data from a url.
- * @param {string} url - The url to convert
- * @returns {string} The splat url
- */
-export function getSplat (url) {
-  if (typeof url !== 'string') {
-    throw new TypeError('URL must be a string')
-  }
-
-  const splat = url.split('/')
-    /* replaces each segment with a splat */
-    .map((seg) => seg.match(/^:/) ? '*' : seg)
-    .join('/')
-
-  return { url, splat }
-}
-
-/**
- * Extracts segment data from a url using a splat.
- * @param {string} url - The url to parse
- * @param {string} splatter - The url splatter (inludes url and splat)
- * @returns {object} The segment data
- */
-export function getSegmentData (url, splatter) {
-  if (typeof url !== 'string') {
-    throw new TypeError('URL must be a string')
-  }
-  if (typeof splatter !== 'object') {
-    throw new TypeError('Splatter must be an object')
-  }
-
-  const urlSplit = url.split('/')
-  const splatterUrlSplit = splatter.url.split('/')
-
-  const data = {}
-
-  splatter.splat.split('/').forEach((seg, indx) => {
-    if (seg === '*') {
-      data[splatterUrlSplit[indx].substring(1)] = urlSplit[indx]
-    }
-  })
-
-  return data
+  /* add route to route map */
+  config().routeMap[method][regexUrl] = config
 }
